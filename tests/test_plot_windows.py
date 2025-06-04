c8b8d2dda61527388524ccfaa03ab0e98fcb49ae
Mat Haskell <mhaskell9@gmail.com>
2025-06-04 15:02:49 -0600

    Update tests to run with new interface

--- tests/test_plot_windows.py
+++ tests/test_plot_windows.py
@@ -4,8 +4,8 @@ from mpl_qt_tabbed_plots import TabbedPlotWindow
 
 
 def test_tabbed_plot_window():
-    window1 = TabbedPlotWindow(window_title='Plot Window 1', open_window=True)
-    window2 = TabbedPlotWindow(size=(500,400), open_window=True)
+    window1 = TabbedPlotWindow('test')
+    window2 = TabbedPlotWindow(size=(500,400))
 
     # data
     t = np.arange(0, 10, 0.001)
@@ -13,37 +13,34 @@ def test_tabbed_plot_window():
     ycos = np.cos(t)
 
 
-    f = plt.figure(layout='tight')
+    f = window1.add_figure_tab("sin")
     ax = f.add_subplot()
     line1, = ax.plot(t, ysin, '--')
     ax.set_xlabel('time')
     ax.set_ylabel('sin(t)')
     ax.set_title('Plot of sin(t)')
-    window1.addTab("sin", f)
 
-    f = plt.figure()
+
+    f = window1.add_figure_tab("time")
     ax = f.add_subplot()
     ax.plot(t, t)
     ax.set_xlabel('time')
     ax.set_ylabel('t')
     ax.set_title('Plot of t')
-    window1.addTab("time", f)
 
-    f = plt.figure()
+    f = window2.add_figure_tab("cos")
     ax = f.add_subplot()
     line2, = ax.plot(t, ycos, '--')
     ax.set_xlabel('time')
     ax.set_ylabel('cos(t)')
     ax.set_title('Plot of cos(t)')
-    window2.addTab("cos", f)
 
-    f = plt.figure()
+    f = window2.add_figure_tab("time")
     ax = f.add_subplot()
     ax.plot(t, t)
     ax.set_xlabel('time')
     ax.set_ylabel('t')
     ax.set_title('Plot of t', fontsize=20)
-    window2.addTab("time", f)
 
     # animate
     dt = 0.1
@@ -58,3 +55,6 @@ def test_tabbed_plot_window():
 
     TabbedPlotWindow.show_all(block=False)
 
+
+if __name__ == "__main__":
+    test_tabbed_plot_window()
